2c183bde10d7bce63dd10ab75444cabb27493a6f
carlynmayes <cmayes@svsu.edu>
2026-05-01 00:26:50 -0400

    Add console log for login response data
    Log the JSON response data for debugging purposes.

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -26,6 +26,7 @@ if (registerForm) {
 
       // Parse the JSON response body from the server
       const data = await res.json()
+      console.log("LOGIN DATA:", data)
 
       if (!res.ok) {
         // res.ok is false when the status is 4xx or 5xx
